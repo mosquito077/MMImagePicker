a85b60253aa5554a017797b99f6a37d9c0bb2005
mosquito <wenwen.zhang@yoho.cn>
2017-08-09 11:17:51 +0800

    Initial Commit

---
+++ MMImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MMImagePicker
+//
+//  Created by mosquito on 2017/8/9.
+//  Copyright © 2017年 mosquito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
